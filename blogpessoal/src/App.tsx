3f4cdec11031f97b877307bff13d1952968f6502
EduAleixo <eduarda.aleixo07@gmail.com>
2026-05-08 17:21:51 -0300

    feat: instalação do tailwind e estilização padrão do blog pessoal

--- blogpessoal/src/App.tsx
+++ blogpessoal/src/App.tsx
@@ -1,10 +1,17 @@
 import './App.css'
+import Footer from './components/footer/Footer'
+import Navbar from './components/navbar/Navbar'
 import Home from './pages/home/Home'
 
 function App() {
 
   return (
-    <Home/> // Componente Home renderizado dentro do componente App, para que o index consiga renderizar na pág
+    <>
+      <Navbar/>
+      <Home/> {/* Componente Home renderizado dentro do componente App, para que o index consiga renderizar na página */ }
+      <Footer/>
+
+    </>
   )
 }
 
