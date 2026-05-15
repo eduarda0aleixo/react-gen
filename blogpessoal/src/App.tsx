9b76110b9ffc39d7aab8246a0ce740cf2e9648d4
EduAleixo <eduarda.aleixo07@gmail.com>
2026-05-14 23:40:57 -0300

    Estilização CardTema e Lista Temas

--- blogpessoal/src/App.tsx
+++ blogpessoal/src/App.tsx
@@ -6,6 +6,7 @@ import Home from './pages/home/Home'
 import Cadastro from './pages/cadastro/Cadastro'
 import Login from './pages/login/Login'
 import { AuthProvider } from './contexts/AuthContext'
+import ListaTemas from './components/tema/listatemas/ListaTemas'
 
 function App() {
   return (
@@ -17,8 +18,9 @@ function App() {
           <div className="grow">
             <Routes>
               <Route path="/login" element={<Login />} />
-              <Route path="/cadastro" element={<Cadastro />} />
               <Route path='/home' element={<Home />} />
+              <Route path="/cadastro" element={<Cadastro />} />
+              <Route path="/temas" element={<ListaTemas />} />
             </Routes>
           </div>
           <Footer />
--- blogpessoal/src/components/navbar/Navbar.tsx
+++ blogpessoal/src/components/navbar/Navbar.tsx
@@ -24,7 +24,7 @@ export default function Navbar(){
             <Link to='/home' className="text-2xl font-bold">Blog Pessoal</Link>
             <div className="flex gap-4">
                 Postagens
-                Temas
+                <Link to='/temas' className='hover:underline'>Temas</Link>
                 Cadastrar tema
                 Perfil
                 <Link to='/login' onClick={logout} className='hover:underline'>
